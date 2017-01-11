d55f30a0146febc944a9c22e95a37d8946276ac6
Jason Milam <jasonlmilam@gmail.com>
2017-01-11 14:14:59 -0500

    Add First Name & Last and remove delete link from admin

--- app/controllers/user_answers/admin_controller.rb
+++ app/controllers/user_answers/admin_controller.rb
@@ -6,7 +6,7 @@ class UserAnswers::AdminController < ApplicationController
 			User.all.each do |user|
 				correct_count = UserAnswer.calculate_correct_count(user.user_answers)
 				total_answers = Answer.where(test_num: user.test_num).count
-				@users["#{user.email}"] = {id: user.id, correct_count: correct_count, total_answers: total_answers, percent: UserAnswer.calculate_percent(correct_count, total_answers), completed_date: user.completed_date}
+				@users["#{user.email}"] = {id: user.id, first_name: user.first_name, last_name: user.last_name, correct_count: correct_count, total_answers: total_answers, percent: UserAnswer.calculate_percent(correct_count, total_answers), completed_date: user.completed_date}
 			end
 		rescue Exception => e 
 		  p "Exception #{e}"
